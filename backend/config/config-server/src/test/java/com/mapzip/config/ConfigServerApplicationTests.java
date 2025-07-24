069ec8bd3da47d8c71144b814181ef8e0d116358
조성욱 <>
2025-07-24 10:54:45 +0900

    Add Config Server implementation and configuration files

---
+++ backend/config/config-server/src/test/java/com/mapzip/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapzip.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
