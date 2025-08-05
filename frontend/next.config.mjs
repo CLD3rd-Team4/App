091822b258c020d6f1d1bc83f4af66baa17a8f26
kaiju782 <didwjdah7@sunmoon.ac.kr>
2025-08-05 14:08:44 +0900

    fix: 정적 Export로 수정

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   env: {
     NEXT_PUBLIC_KAKAO_MAP_KEY: process.env.NEXT_PUBLIC_KAKAO_MAP_KEY,
   },
