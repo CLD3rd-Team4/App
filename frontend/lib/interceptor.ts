8d9f9ca4c35cf1073ccb8f625b8cc21d1f829583
siiiirru <eqeee@naver.com>
2025-08-14 11:50:39 +0900

    fix: 인터셉터 인증실패반환시 로그인여부 세션스토리지 삭제

--- frontend/lib/interceptor.ts
+++ frontend/lib/interceptor.ts
@@ -94,6 +94,7 @@ api.interceptors.response.use(
         }
 
         if (status === 401 && data?.error === "TOKEN_INVALID") {
+            sessionStorage.removeItem("kakaoLoginDone")
             alert("인증되지 않은 사용자입니다. 로그인해주세요.")
             window.location.href = "/login.html"
             return Promise.reject(error)
