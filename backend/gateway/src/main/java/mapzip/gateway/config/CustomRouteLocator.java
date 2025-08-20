a6d1282401a15332e97043ae75326449fd93e3c0
csm123455 <73813506+csm123455@users.noreply.github.com>
2025-08-21 04:51:51 +0900

    fix:게이트웨이 리뷰 상세조회 http 접두사추가

--- backend/gateway/src/main/java/mapzip/gateway/config/CustomRouteLocator.java
+++ backend/gateway/src/main/java/mapzip/gateway/config/CustomRouteLocator.java
@@ -73,7 +73,7 @@ public class CustomRouteLocator {
                                 .filter(jwtAuthenticationFilter.apply(new JwtAuthenticationFilter.Config())))
                         .uri("http://review.service-review:8080"))
             
-                .route("review-http-detail", r -> r.path("/review/{restaurantId}/{reviewId}") // 상세조회 수정 삭제
+                .route("review-http-detail", r -> r.path("/http/review/{restaurantId}/{reviewId}") // 상세조회 수정 삭제
                        .and().method("GET", "PUT", "DELETE")
                        .filters(f -> f
                                 .filter(xssProtectionFilter.apply(new XssProtectionFilter.Config()))
