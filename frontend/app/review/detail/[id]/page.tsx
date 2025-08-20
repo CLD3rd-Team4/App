2c2a0cadada7a95b6058227f5798bd7539de92e5
siiiirru <eqeee@naver.com>
2025-08-20 21:21:42 +0900

    fix:정적 경로생성 삭제

--- frontend/app/review/detail/[id]/page.tsx
+++ frontend/app/review/detail/[id]/page.tsx
@@ -1,13 +1,13 @@
 import ReviewDetailPageClient from "./ReviewDetailPageClient"
 
-export async function generateStaticParams() {
-  // 가능한 모든 리뷰 ID를 미리 생성
-  const ids = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+// export async function generateStaticParams() {
+//   // 가능한 모든 리뷰 ID를 미리 생성
+//   const ids = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 
-  return ids.map((id) => ({
-    id: id,
-  }))
-}
+//   return ids.map((id) => ({
+//     id: id,
+//   }))
+// }
 
 export default function ReviewDetailPage() {
   return <ReviewDetailPageClient />
